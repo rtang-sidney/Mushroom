ef8388321c4c7693222d10fb0f695f05fe9137fb
rtang <rtang@e21-99.e21.frm2>
2019-12-06 17:25:17 +0100

    The method plot_whole_geometry has been updated but not checked yet.

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-energy_in_meV = 8.0
-energy_in_eV = energy_in_meV * 1e-3
-energy_in_SI = energy_in_eV * 1.602176634e-19
-hbar = 1.0545718e-34  # m2 kg / s
-mass = 1.67492749804e-27  # kg
-wavelength = np.pi * hbar * np.sqrt(2. / (mass * energy_in_SI))
-wavevector = 2 * np.pi / wavelength
-print(wavelength * 1e10)
-print(wavevector * 1e-10)
+
+def analyser(number):
+    analyser_x = np.random.rand(number)
+    analyser_y = np.random.rand(number)
+    return analyser_x, analyser_y
+
+
+num = 100
+analysers = analyser(number=num)
+print(analysers[0][0])

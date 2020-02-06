db4fed2615b139a45b5464d329340c3ff9a7a72b
rtang-sidney <ran.tang@frm2.tum.de>
2020-02-06 17:51:56 +0100

    Update instrument parameters

--- helper.py
+++ helper.py
@@ -1,5 +1,16 @@
-import numpy
 import numpy as np
+
+
+class InstrumentContext(object):
+    def __init__(self):
+        self.moasic_pg002 = np.deg2rad(0.4)  # radian, PG crystal mosaic
+        self.moasic_analyser = np.deg2rad(0.8)  # radian, analyser mosaic
+        self.deltad_d = 6e-4  # relative uncertainty of the lattice distance, given in [paper2]
+        self.lattice_distance_pg002 = 3.35e-10  # m, lattice distance d of a PG crystal
+        self.analyser_segment = 1e-2  # m, the size of an analyser segment in 1D
+        self.distance_ms = 1.0  # m, distance between the monochromator and sample
+        self.divergence_initial = np.deg2rad(1.6)  # initial divergence directly from the neutron guide
+
 
 MASS_NEUTRON = 1.67492749804e-27  # kg
 PLANCKS_CONSTANT = 1.0545718e-34  # m2 kg / s
@@ -37,3 +48,8 @@ def vector_bisector(vector1, vector2):
     vector1 = np.array(vector1)
     vector2 = np.array(vector2)
     return np.linalg.norm(vector1) * vector2 + np.linalg.norm(vector2) * vector1
+
+
+def wavenumber_to_2theta_bragg(instrument: InstrumentContext, wave_number, ordering=1):
+    # returns the scattering angle 2theta according to the Bragg's law 2 * d * sin(theta) = n * lambda
+    return 2.0 * np.arcsin(ordering * np.pi / (wave_number * instrument.lattice_distance_pg002))
